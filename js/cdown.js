a5ee380e35fac8702e2006462782607cd5634cc9
abtsousa <afonso.sousa@gmail.com>
2020-10-15 19:53:56 +0100

    Update cdown.js

--- js/cdown.js
+++ js/cdown.js
@@ -6,7 +6,7 @@ $(function () {
      ========================================= */
     $('#clock').countdown('2020/11/20 14:00').on('update.countdown', function(event) {
       var $this = $(this).html(event.strftime(''
-        + '<span class="h1 text-white font-weight-bold">%D</span> Dia%!d'
+        + '<span class="h1 text-white font-weight-bold">%D</span> Dia%!D'
         + '<span class="h1 text-white font-weight-bold">%H</span> Hr'
         + '<span class="h1 text-white font-weight-bold">%M</span> Min'
         + '<span class="h1 text-white font-weight-bold">%S</span> Seg'));
