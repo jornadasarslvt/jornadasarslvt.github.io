680acef42d886927722d6f479413079db2485c87
abtsousa <afonso.sousa@gmail.com>
2021-03-05 23:38:32 +0000

    v2.0 -- Updated for main event

--- js/cdown.js
+++ js/cdown.js
@@ -4,7 +4,7 @@ $(function () {
     /* =========================================
         COUNTDOWN 1
      ========================================= */
-    $('#clock').countdown('2020/11/20 14:00').on('update.countdown', function(event) {
+    $('#clock').countdown('2021/07/01 09:00').on('update.countdown', function(event) {
       var $this = $(this).html(event.strftime(''
         + '<span class="h1 text-white font-weight-bold">%D</span> Dia%!D'
         + '<span class="h1 text-white font-weight-bold">%H</span> Hr'
